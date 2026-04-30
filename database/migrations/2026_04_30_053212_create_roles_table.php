06c015f4ed94e6472a609731b8c7c608611eec2a
meda63699 <meda63699@gmail.com>
2026-04-30 08:33:12 +0300

    Create migration file for roles table

---
+++ database/migrations/2026_04_30_053212_create_roles_table.php
@@ -0,0 +1,36 @@
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+
+        // Insert default roles
+        DB::table('roles')->insert([
+            ['name' => 'Admin', 'description' => null],
+            ['name' => 'Sales', 'description' => null],
+            ['name' => 'Support', 'description' => null],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
