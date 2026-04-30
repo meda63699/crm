931c21c701b51b60a43f179da8ecfeae23582a06
meda63699 <meda63699@gmail.com>
2026-04-30 08:46:21 +0300

    Create employees migration table

---
+++ database/migrations/2026_04_30_054510_create_employees_table.php
@@ -0,0 +1,39 @@
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Schema;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('role_id');
+            $table->string('job_title');
+            $table->string('department');
+            $table->decimal('salary', 8, 2);
+            $table->date('hire_date');
+            $table->timestamps();
+            
+            // Foreign key constraints can be added here
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
